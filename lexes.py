10222634ceda5247e6927650a12032384f47b667
Alex Ho <alexanderh6886@gmail.com>
2025-06-25 12:23:26 +1000

    First development. Entry class started.

--- lexes.py
+++ lexes.py
@@ -1,4 +1,21 @@
-# Lexes
-print("Demo")
-print("Push test")
-# PC TEST
+### Program: Lexes
+### Author: Alexander Ho (ho-0084@mhs.vic.edu.au)
+### Date Created: 25/06/2025
+### Date Finished: TBA  
+### Description: Personal dictionary and flashcard creation solution for students. Created for VCE Software Development 3&4 SAT.
+
+import sqlite3
+import customtkinter as ctk
+from tkinter import messagebox
+import datetime
+
+class Entry:
+    # Initiates the Entry with its attributes
+    def __init__(self, uid: int = None, term: str, definition: str, tags: str, createdAt: datetime.datetime = None):
+        self.term = term
+        self.definition = definition
+        self.tags = tags
+        self.createdAt = createdAt
+    
+    def add():
+        pass
