4e3df472e82824a97d057678dcdfb9ca1786324d
Alex Ho <alexanderh6886@gmail.com>
2025-06-25 20:25:33 +1000

    Created class definitions with all attributes. Setup db

--- lexes.py
+++ lexes.py
@@ -8,10 +8,15 @@ import sqlite3
 import customtkinter as ctk
 from tkinter import messagebox
 import datetime
+import os
 
 class Entry:
     # Initiates the Entry with its attributes: uid and createdAt are optional parameters.
-    def __init__(self, term: str, definition: str, tags: str, createdAt: datetime.datetime | None = None, uid: int | None = None):
+    def __init__(self, term: str,
+                 definition: str,
+                 tags: str,
+                 createdAt: datetime.datetime | None = None,
+                 uid: int | None = None):
         self.uid = uid
         self.term = term
         self.definition = definition
@@ -20,3 +25,57 @@ class Entry:
             
     def add():
         pass
+
+    def edit():
+        pass
+
+    def delete():
+        pass
+
+    def autoGenerate():
+        pass
+
+    def select():
+        pass
+
+    def unselect():
+        pass
+
+class DisplayList:
+    def __init__(self, entries: list | None = None,
+                 filterTags: str | None = None,
+                 requireAllTags: bool | None = None,
+                 searchKeyword: str | None = None,
+                 sortAttribute: str | None = None):
+        self.entries = entries
+        self.filterTags = filterTags
+        self.requireAllTags = requireAllTags
+        self.searchKeyword = searchKeyword
+        self.sortAttribute = sortAttribute
+
+class SelectedList:
+    def __init__(self, entries: list | None = None):
+        self.entries = entries
+
+class ImportList:
+    def __init__(self, rawText: str | None = None,
+                 entryDelimiter: str | None = None,
+                 termDefinitionDelimiter: str | None = None,
+                 massTags: str | None = None,
+                 parsedEntries: list | None = None,
+                 filePath: str | None = None):
+        self.rawText = rawText
+        self.entryDelimiter = entryDelimiter
+        self.termDefinitionDelimiter = termDefinitionDelimiter
+        self.massTags = massTags
+        self.parsedEntries = parsedEntries
+        self.filePath = filePath
+
+class App:
+    def __init__(self, dbPath: str):
+        self.dbPath = dbPath
+        conn = sqlite3.connect(dbPath)
+        conn.close()
+
+App(dbPath=r"assets\lexes.db")
+        
