77cad5347f1c422af7a3c9ad43c66b0c2b97253b
Alex Ho <alexanderh6886@gmail.com>
2025-06-25 22:15:20 +1000

    Entry class done. Helper WikipediaAPI method done. SQL works with entry methods.

--- lexes.py
+++ lexes.py
@@ -8,7 +8,21 @@ import sqlite3
 import customtkinter as ctk
 from tkinter import messagebox
 import datetime
-import os
+import requests
+
+class Helper:
+    @staticmethod
+    # Static method to call Wikipedia API, parse response, and return definition (if found).
+    def wikipediaAPI(query: str) -> str:
+        url = f"https://en.wikipedia.org/api/rest_v1/page/summary/{query}"
+        response = requests.get(url)
+        if response.status_code == 200: # successful request
+            data = response.json()
+            extract = data.get("extract")
+            return str(extract) if extract is not None else None
+        else:
+            return None
+
 
 class Entry:
     # Initiates the Entry with its attributes: uid and createdAt are optional parameters.
@@ -22,24 +36,64 @@ class Entry:
         self.definition = definition
         self.tags = tags
         self.createdAt = createdAt
-            
-    def add():
-        pass
+    
+    # Custom representation of what the entry object looks like when printed.
+    def __repr__(self):
+        return f"Entry(uid={self.uid}, term={self.term}, definition={self.definition}, tags={self.tags}, createdAt={self.createdAt})"
 
-    def edit():
-        pass
+    # Creates timestamp of YYYY:MM:DD HH:MM:SS and pushes entry into DB
+    def add(self):
+        self.createdAt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        
+        conn = sqlite3.connect(app.dbPath)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO master (term, definition, tags, createdAt) VALUES (?, ?, ?, ?)",
+                       (self.term, self.definition, self.tags, self.createdAt))
+        conn.commit()
+        conn.close()
 
-    def delete():
-        pass
+    # Updates entry attributes then updates row in DB where uid matches.
+    def edit(self, newTerm: str, newDefinition: str, newTags: str):
+        self.term = newTerm
+        self.definition = newDefinition
+        self.tags = newTags
+        uid = self.uid
 
-    def autoGenerate():
-        pass
+        conn = sqlite3.connect(app.dbPath)
+        cursor = conn.cursor()
+        cursor.execute("UPDATE master SET term = ?, definition = ?, tags = ? WHERE uid = ?",
+                       (self.term, self.definition, self.tags, uid))
+        conn.commit()
+        conn.close()
+    
+    # Deletes row with matching uid.
+    # NOTE: Does not need to remove from displayList and selectedList to separate functionality from UI. (Button functions will manage this.)
+    def delete(self):
+        uid = self.uid
 
-    def select():
-        pass
+        conn = sqlite3.connect(app.dbPath)
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM master WHERE uid = ?",
+                       (uid,))
+        conn.commit()
+        conn.close()
+    
+    # Retrieves definition from Wikipedia API using helper function, then returns string as string.
+    def autoGenerate(self) -> str:
+        retrievedDefinition = Helper.wikipediaAPI(self.term)
+        return retrievedDefinition if retrievedDefinition is not None else ""
 
-    def unselect():
-        pass
+    # Adds entry to selectedList if not in it.
+    # NOTE: SelectedList object must be passed as parameter.
+    def select(self, selectedList: 'SelectedList'):
+        if self not in selectedList.entries:
+            selectedList.entries.append(self)
+
+    # Removes entry from selectedList if in it.
+    # NOTE: SelectedList object must be passed as parameter.
+    def unselect(self, selectedList: 'SelectedList'):
+        if self in selectedList.entries:
+            selectedList.entries.remove(self)
 
 class DisplayList:
     def __init__(self, entries: list | None = None,
@@ -76,8 +130,6 @@ class App:
         self.dbPath = dbPath
         conn = sqlite3.connect(dbPath)
         self.setupDB()
-
-        conn.close()
     
     def setupDB(self):
         conn = sqlite3.connect(self.dbPath)
@@ -92,7 +144,11 @@ class App:
             createdAt TEXT NOT NULL)
         """)
         conn.commit()
+        conn.close()
 
+# Testing
+if __name__ == "__main__":
+    app = App(dbPath=r"assets\lexes.db")
 
-App(dbPath=r"assets\lexes.db")
-        
+    entry = Entry("testTerm", "testDefinition", "testTags")
+    entry.add()
